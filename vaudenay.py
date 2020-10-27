da6199fa58beac6e06609bfc76f2e3bff837c23e
Igor Martayan <igor@martayan.org>
2020-10-27 19:35:52 +0100

    Improved Vaudenay's attack, we can now completely reverse the encrypted message.

--- vaudenay.py
+++ vaudenay.py
@@ -1,5 +1,4 @@
 from sys import argv
-from random import randint
 from subprocess import call, DEVNULL
 
 # Initialization vector and secret key used by AES
@@ -18,7 +17,7 @@ basename = ".".join(source.split(".")[:-1])
 extension = "." + source.split(".")[-1]
 source_cpy = basename + "_cpy" + extension
 
-# Creating a list with the encrypted bytes
+# List of the encrypted bytes
 with open(source, "rb") as f:
     enc = list(f.read())
 
@@ -50,39 +49,96 @@ def try_decrypt(content):
     return call(cmd, stdout=DEVNULL, stderr=DEVNULL)
 
 
-content = enc[:]
-# Finding the padding size
-p = block_size
-content[-block_size - p] ^= 1
-while try_decrypt(content) == 0:
-    p -= 1
+def find_padding(enc):
+    """
+    Finds the padding size of the message.
+    """
+    content = enc[:]
+    p = block_size
     content[-block_size - p] ^= 1
-padding_size = p
+    while try_decrypt(content) == 0:
+        p -= 1
+        content[-block_size - p] ^= 1
+    return p
+
 
 dec = [None] * nb_bytes
-for i in range(1, padding_size + 1):
-    dec[-i] = enc[-block_size - i] ^ padding_size
-
+padding_size = find_padding(enc)
 actual_size = nb_bytes - padding_size
 msg = [None] * actual_size
 
-content = enc[:]
-# Breaking the last block
-for i in range(1, block_size - padding_size + 1):
-    p = padding_size + i
-    for j in range(1, p):
-        content[-block_size - j] ^= (p - 1) ^ p
-    for k in range(256):
-        content[-block_size - p] = k
-        if try_decrypt(content) == 0:
-            dec[-i] = k ^ p
-            msg[-i] = dec[-i] ^ enc[-block_size - p]
-            print(f"Found letter {-i}: {chr(msg[-i])}")
+
+def reverse_last_block(enc):
+    """
+    Reverses the last block of the message.
+    """
+    content = enc[:]
+    for p in range(1, padding_size + 1):
+        dec[-p] = enc[-p - block_size] ^ padding_size
+    for p in range(padding_size + 1, block_size + 1):
+        i = nb_bytes - p
+        for j in range(1, p):
+            content[nb_bytes - block_size - j] ^= (p - 1) ^ p
+        for k in range(256):
+            content[i - block_size] = k
+            if try_decrypt(content) == 0:
+                dec[i] = k ^ p
+                msg[i] = chr(dec[i] ^ enc[i - block_size])
+                break
+        else:
+            print(f"Failed to reverse byte {i}")
             break
     else:
-        print(f"Failed to reverse byte {-i}")
+        print("Reversed last block")
 
 
-with open("msg.txt", "rb") as f:
-    sol = list(f.read())
-# Expected output, to compare with what we found
+def reverse_block(enc, b):
+    """
+    Reverses the block number b of the message.
+    """
+    assert 2 <= b < nb_blocks
+    content = enc[: b * block_size]
+    for p in range(1, block_size + 1):
+        i = b * block_size - p
+        for j in range(1, p):
+            content[(b - 1) * block_size - j] ^= (p - 1) ^ p
+        for k in range(256):
+            content[i - block_size] = k
+            if try_decrypt(content) == 0:
+                dec[i] = k ^ p
+                msg[i] = chr(dec[i] ^ enc[i - block_size])
+                break
+        else:
+            print(f"Failed to reverse byte {i}")
+            break
+    else:
+        print(f"Reversed block n°{b}")
+
+
+def reverse_first_block(enc):
+    """
+    Reverses the first block of the message using the IV.
+    """
+    iv = list(bytes.fromhex(aes_iv))
+    content = iv + enc[:block_size]
+    for p in range(1, block_size + 1):
+        i = block_size - p
+        for j in range(1, p):
+            content[block_size - j] ^= (p - 1) ^ p
+        for k in range(256):
+            content[i] = k
+            if try_decrypt(content) == 0:
+                dec[i] = k ^ p
+                msg[i] = chr(dec[i] ^ iv[i])
+                break
+        else:
+            print(f"Failed to reverse byte {i}")
+            break
+    else:
+        print(f"Reversed first block")
+
+
+reverse_last_block(enc)
+for b in range(nb_blocks - 1, 1, -1):
+    reverse_block(enc, b)
+reverse_first_block(enc)
