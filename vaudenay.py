7ebcf226be49ee17d24ef4a326d9df05b76a3461
Igor Martayan <igor@martayan.org>
2020-11-05 18:43:18 +0100

    Use the pycrypto module for AES decryption, Vaudenay's attack is now much faster. Merged reverse_block and reverse_first_block by adding the IV at the beginning of the message.

--- vaudenay.py
+++ vaudenay.py
@@ -1,11 +1,12 @@
 from sys import argv
-from subprocess import call, DEVNULL
+from Crypto.Cipher import AES
 
 # Initialization vector and secret key used by AES
 aes_iv = "12345678901234567890123456789012"
 aes_key = "23456789012345678901234567890123"
-aes_size = 128  # size of one block (in bits)
-block_size = aes_size // 8  # idem (in bytes)
+iv = bytes.fromhex(aes_iv)
+key = bytes.fromhex(aes_key)
+block_size = 128 // 8  # size of one block (in bytes)
 
 # Name of the encrypted file
 if len(argv) < 2:
@@ -13,13 +14,10 @@ if len(argv) < 2:
 else:
     source = argv[1]
 
-basename = ".".join(source.split(".")[:-1])
-extension = "." + source.split(".")[-1]
-source_cpy = basename + "_cpy" + extension
-
 # List of the encrypted bytes
 with open(source, "rb") as f:
-    enc = list(f.read())
+    # add the initialization vector at the beginning
+    enc = list(iv) + list(f.read())
 
 nb_bytes = len(enc)
 nb_blocks = nb_bytes // block_size
@@ -28,25 +26,13 @@ nb_blocks = nb_bytes // block_size
 def try_decrypt(content):
     """
     Oracle that tries to decrypt the content given in argument
-    using AES-128-CBC with a fixed key and initialization vector.
-    Returns 0 when the content is valid, 1 otherwise.
+    using AES-128-CBC with a fixed key and initialization vector,
+    and returns whether the padding is valid or not.
     """
-    with open(source_cpy, "wb") as f:
-        f.write(bytes(content))
-    # Problem : it is quite slow to rewrite everything
-    cmd = [
-        "openssl",
-        "enc",
-        "-d",
-        "-aes-128-cbc",
-        "-in",
-        source_cpy,
-        "-iv",
-        aes_iv,
-        "-K",
-        aes_key,
-    ]
-    return call(cmd, stdout=DEVNULL, stderr=DEVNULL)
+    aes = AES.new(key, AES.MODE_CBC, iv)
+    res = list(aes.decrypt(bytes(content)))
+    p = res[-1]
+    return res[-p:] == [p] * p
 
 
 def find_padding(enc):
@@ -56,16 +42,17 @@ def find_padding(enc):
     content = enc[:]
     p = block_size
     content[-block_size - p] ^= 1
-    while try_decrypt(content) == 0:
+    while try_decrypt(content):
         p -= 1
         content[-block_size - p] ^= 1
     return p
 
 
-dec = [None] * nb_bytes
+dec_size = nb_bytes - block_size
+dec = [None] * dec_size
 padding_size = find_padding(enc)
-actual_size = nb_bytes - padding_size
-msg = [None] * actual_size
+actual_size = dec_size - padding_size
+msg = ["?"] * actual_size
 
 
 def reverse_last_block(enc):
@@ -76,14 +63,14 @@ def reverse_last_block(enc):
     for p in range(1, padding_size + 1):
         dec[-p] = enc[-p - block_size] ^ padding_size
     for p in range(padding_size + 1, block_size + 1):
-        i = nb_bytes - p
+        i = dec_size - p
         for j in range(1, p):
-            content[nb_bytes - block_size - j] ^= (p - 1) ^ p
+            content[dec_size - j] ^= (p - 1) ^ p
         for k in range(256):
-            content[i - block_size] = k
-            if try_decrypt(content) == 0:
+            content[i] = k
+            if try_decrypt(content):
                 dec[i] = k ^ p
-                msg[i] = chr(dec[i] ^ enc[i - block_size])
+                msg[i] = chr(dec[i] ^ enc[i])
                 break
         else:
             print(f"Failed to reverse byte {i}")
@@ -99,46 +86,25 @@ def reverse_block(enc, b):
     assert 2 <= b < nb_blocks
     content = enc[: b * block_size]
     for p in range(1, block_size + 1):
-        i = b * block_size - p
+        i = (b - 1) * block_size - p
         for j in range(1, p):
             content[(b - 1) * block_size - j] ^= (p - 1) ^ p
         for k in range(256):
-            content[i - block_size] = k
-            if try_decrypt(content) == 0:
-                dec[i] = k ^ p
-                msg[i] = chr(dec[i] ^ enc[i - block_size])
-                break
-        else:
-            print(f"Failed to reverse byte {i}")
-            break
-    else:
-        print(f"Reversed block n°{b}")
-
-
-def reverse_first_block(enc):
-    """
-    Reverses the first block of the message using the IV.
-    """
-    iv = list(bytes.fromhex(aes_iv))
-    content = iv + enc[:block_size]
-    for p in range(1, block_size + 1):
-        i = block_size - p
-        for j in range(1, p):
-            content[block_size - j] ^= (p - 1) ^ p
-        for k in range(256):
             content[i] = k
-            if try_decrypt(content) == 0:
+            if try_decrypt(content):
                 dec[i] = k ^ p
-                msg[i] = chr(dec[i] ^ iv[i])
+                msg[i] = chr(dec[i] ^ enc[i])
                 break
         else:
             print(f"Failed to reverse byte {i}")
             break
     else:
-        print(f"Reversed first block")
+        print(f"Reversed block n°{b-1}")
 
 
 reverse_last_block(enc)
 for b in range(nb_blocks - 1, 1, -1):
     reverse_block(enc, b)
-reverse_first_block(enc)
+
+print("Decrypted message:")
+print("".join(msg))
