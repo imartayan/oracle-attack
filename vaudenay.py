3ab6a99c9cb8f1472ca4608b74b6c866e85472cd
Igor Martayan <igor@martayan.org>
2020-11-22 18:18:15 +0100

    Use argparse for parsing arguments, add option to encrypt clear message

--- vaudenay.py
+++ vaudenay.py
@@ -1,36 +1,71 @@
-from sys import argv
+from argparse import ArgumentParser
 from Crypto.Cipher import AES
 
-# Initialization vector and secret key used by AES
-aes_iv = "12345678901234567890123456789012"
-aes_key = "23456789012345678901234567890123"
-iv = bytes.fromhex(aes_iv)
-key = bytes.fromhex(aes_key)
-block_size = 128 // 8  # size of one block (in bytes)
+parser = ArgumentParser(
+    description="Python implementation of Vaudenay's oracle attack on AES-128-CBC"
+)
+parser.add_argument("file", help="file containing the (encrypted) message")
+parser.add_argument("-iv", help="initialization vector")
+parser.add_argument("-k", "--key", help="secret key used for encryption/decryption")
+parser.add_argument(
+    "-s",
+    "--size",
+    help="AES block size in bits (128 by default)",
+    default=128,
+    type=int,
+)
+parser.add_argument(
+    "-e",
+    "--encrypt",
+    help="encrypt the message with iv and secret key",
+    default=False,
+    action="store_true",
+)
+args = parser.parse_args()
 
-# Name of the encrypted file
-if len(argv) < 2:
-    source = "enc.txt"
+if args.iv:
+    aes_iv = args.iv
 else:
-    source = argv[1]
+    aes_iv = "12345678901234567890123456789012"
+iv = bytes.fromhex(aes_iv)
 
-# List of the encrypted bytes
+if args.key:
+    aes_key = args.key
+else:
+    aes_key = "23456789012345678901234567890123"
+key = bytes.fromhex(aes_key)
+
+# size of one block (in bytes)
+block_size = args.size // 8
+
+if args.encrypt:
+    with open(args.file, "r") as f:
+        content = bytes(f.read(), "utf-8")
+    # padding
+    r = len(content) % block_size
+    p = block_size - r
+    padding = p.to_bytes(1, "big") * p
+    basename = ".".join(args.file.split(".")[:-1])
+    source = basename + "_aes.enc"
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    with open(source, "wb") as out:
+        out.write(cipher.encrypt(content + padding))
+else:
+    source = args.file
+
 with open(source, "rb") as f:
     # add the initialization vector at the beginning
     enc = list(iv) + list(f.read())
 
-nb_bytes = len(enc)
-nb_blocks = nb_bytes // block_size
 
-
-def try_decrypt(content):
+def oracle(content):
     """
-    Oracle that tries to decrypt the content given in argument
+    Oracle that tries to decrypt the message given in argument
     using AES-128-CBC with a fixed key and initialization vector,
     and returns whether the padding is valid or not.
     """
-    aes = AES.new(key, AES.MODE_CBC, iv)
-    res = list(aes.decrypt(bytes(content)))
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    res = list(cipher.decrypt(bytes(content)))
     p = res[-1]
     return res[-p:] == [p] * p
 
@@ -42,12 +77,14 @@ def find_padding(enc):
     content = enc[:]
     p = block_size
     content[-block_size - p] ^= 1
-    while try_decrypt(content):
+    while oracle(content):
         p -= 1
         content[-block_size - p] ^= 1
     return p
 
 
+nb_bytes = len(enc)
+nb_blocks = nb_bytes // block_size
 dec_size = nb_bytes - block_size
 dec = [None] * dec_size
 padding_size = find_padding(enc)
@@ -68,7 +105,7 @@ def reverse_last_block(enc):
             content[dec_size - j] ^= (p - 1) ^ p
         for k in range(256):
             content[i] = k
-            if try_decrypt(content):
+            if oracle(content):
                 dec[i] = k ^ p
                 msg[i] = chr(dec[i] ^ enc[i])
                 break
@@ -91,7 +128,7 @@ def reverse_block(enc, b):
             content[(b - 1) * block_size - j] ^= (p - 1) ^ p
         for k in range(256):
             content[i] = k
-            if try_decrypt(content):
+            if oracle(content):
                 dec[i] = k ^ p
                 msg[i] = chr(dec[i] ^ enc[i])
                 break
@@ -102,6 +139,7 @@ def reverse_block(enc, b):
         print(f"Reversed block n°{b-1}")
 
 
+# Vaudenay's attack
 reverse_last_block(enc)
 for b in range(nb_blocks - 1, 1, -1):
     reverse_block(enc, b)
